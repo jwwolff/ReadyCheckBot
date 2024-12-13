6552c5b7692ae395fb85c704e0b8cd750ff1a298
knie20 <kdnie20@gmail.com>
2024-12-12 20:09:45 -0500

    return the previous commands, shouldn't be modifying behavior

--- src/register-commands.js
+++ src/register-commands.js
@@ -1,55 +1,67 @@
 require('dotenv').config();
 const{REST,Routes, ApplicationCommandOptionType} = require('discord.js');
 
+const options = [
+    {
+        name: 'user1',
+        description: 'First User',
+        type: ApplicationCommandOptionType.User,
+        required: false,
+        autocomplete: true
+    },
+    {
+        name: 'user2',
+        description: 'Second User',
+        type: ApplicationCommandOptionType.User,
+        required: false,
+        autocomplete: true
+    },
+    {
+        name: 'user3',
+        description: 'Third User',
+        type: ApplicationCommandOptionType.User,
+        required: false,
+        autocomplete: true
+    },
+    {
+        name: 'user4',
+        description: 'Fourth User',
+        type: ApplicationCommandOptionType.User,
+        required: false,
+        autocomplete: true
+    },
+    {
+        name: 'user5',
+        description: 'Fifth User',
+        type: ApplicationCommandOptionType.User,
+        required: false,
+        autocomplete: true
+    },
+    {
+        name: 'user6',
+        description: 'Sixth User',
+        type: ApplicationCommandOptionType.User,
+        required: false,
+        autocomplete: true
+    }
+];
+
 const commands = [
+    {
+        name: 'ready-check',
+        description: 'Initiates a ready check for everyone in a voice channel',
+        options
+    },
+    {
+        name: 'readycheck',
+        description: 'Initiates a ready check for everyone in a voice channel',
+        options
+    },
     {
         name: 'rcheck',
         description: 'Initiates a ready check for everyone in a voice channel. Can tag up to 6 users in addition to the caller',
-        options: [
-            {
-                name: 'user1',
-                description: 'First User',
-                type: ApplicationCommandOptionType.User,
-                required: false,
-                autocomplete: true
-            },
-            {
-                name: 'user2',
-                description: 'Second User',
-                type: ApplicationCommandOptionType.User,
-                required: false,
-                autocomplete: true
-            },
-            {
-                name: 'user3',
-                description: 'Third User',
-                type: ApplicationCommandOptionType.User,
-                required: false,
-                autocomplete: true
-            },
-            {
-                name: 'user4',
-                description: 'Fourth User',
-                type: ApplicationCommandOptionType.User,
-                required: false,
-                autocomplete: true
-            },
-            {
-                name: 'user5',
-                description: 'Fifth User',
-                type: ApplicationCommandOptionType.User,
-                required: false,
-                autocomplete: true
-            },
-            {
-                name: 'user6',
-                description: 'Sixth User',
-                type: ApplicationCommandOptionType.User,
-                required: false,
-                autocomplete: true
-            }
-        ]
-    },
+        options
+    }
 ];
 
 const rest = new REST({version: '10'}).setToken(process.env.TOKEN);
@@ -57,11 +69,17 @@ const rest = new REST({version: '10'}).setToken(process.env.TOKEN);
 (async () => {
     try{
         console.log('Registering Commands...');
-
+/*
+        await rest.put(
+            Routes.applicationCommands(process.env.CLIENT_ID),
+            {body: commands}
+        );
+*/
         await rest.put(
             Routes.applicationGuildCommands(process.env.CLIENT_ID,process.env.GUILD_ID),
             {body: commands}
         );
+
 
         console.log('Commands Registered');
         
